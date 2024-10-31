916339db6b22e36b885364809723f90b774acef8
vovan101207 <shiryayev_vova@bk.ru>
2024-10-31 17:20:25 +0300

    1.4

--- controllers/MovieController.js
+++ controllers/MovieController.js
@@ -1,17 +1,31 @@
-import fs from 'fs';
+import fs from 'fs'
 
 export const getMovies = (req, res) => {
 	try {
-		const movies = JSON.parse(fs.readFileSync('./items.json', 'utf8'));
+		const movies = JSON.parse(fs.readFileSync('./items.json', 'utf8'))
+		const { name } = req.query
+
+		if (name) {
+			const matchingMovies = movies.filter(movie =>
+				movie.name.toLowerCase().includes(name.toLowerCase())
+			)
+			if (matchingMovies.length > 0) {
+				return res.json(matchingMovies)
+			} else {
+				return res
+					.status(404)
+					.json({ message: 'No movies found with that name' })
+			}
+		}
 		res.json(movies)
 	} catch (err) {
-		res.status(404).send('Not Found');
+		res.status(404).send('Not Found')
 	}
 }
 
 export const getMovieById = (req, res) => {
 	try {
-		const movies = JSON.parse(fs.readFileSync('./items.json', 'utf8'));
+		const movies = JSON.parse(fs.readFileSync('./items.json', 'utf8'))
 		const id = parseInt(req.params.id)
 		const movie = movies.find(movie => movie.id === id)
 		if (!movie) {
@@ -20,6 +34,6 @@ export const getMovieById = (req, res) => {
 
 		res.json(movie)
 	} catch (err) {
-		res.status(404).send('Not Found');
+		res.status(404).send('Not Found')
 	}
 }
