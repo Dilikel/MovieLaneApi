66492e6569d1592ba794324a3ca014777576321e
vovan101207 <shiryayev_vova@bk.ru>
2024-11-24 12:04:31 +0300

    1.6

--- index.js
+++ index.js
@@ -3,25 +3,9 @@ import connectDB from './config/db.js'
 import authRoutes from './routes/authRoutes.js'
 import movieRoutes from './routes/movieRoutes.js'
 import cors from 'cors'
-import swaggerJSDoc from 'swagger-jsdoc'
-import swaggerUi from 'swagger-ui-express'
 
 const app = express()
 const PORT = process.env.PORT || 8000
-
-const swaggerOptions = {
-	definition: {
-		openapi: '3.0.0',
-		info: {
-			title: 'MovieLane API',
-			version: '1.0.0',
-		},
-	},
-	apis: ['./routes/*.js'],
-}
-
-const swaggerDocs = swaggerJSDoc(swaggerOptions)
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 connectDB()
 
