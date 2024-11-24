e51c83be636ba9e518679e48f3f3e43fb4796591
vovan101207 <shiryayev_vova@bk.ru>
2024-11-24 10:40:39 +0300

    1.5

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -13,4 +13,105 @@ router.post('/login', loginValidation, handleValidationErrors, login)
 router.post('/register', registerValidation, handleValidationErrors, register)
 router.get('/auth', CheckAuth, getMe)
 
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     summary: Login a user
+ *     tags:
+ *       - Authentication
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Successful login
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *       400:
+ *         description: Login failed
+ *       500:
+ *         description: Server error
+ */
+
+/**
+ * @swagger
+ * /register:
+ *   post:
+ *     summary: Register a new user
+ *     tags:
+ *       - Authentication
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               name:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Registration successful
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *       400:
+ *         description: Validation error
+ *       500:
+ *         description: Server error
+ */
+
+/**
+ * @swagger
+ * /auth:
+ *   get:
+ *     summary: Get user information
+ *     tags:
+ *       - Authentication
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: User information retrieved successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: string
+ *                 email:
+ *                   type: string
+ *                 name:
+ *                   type: string
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: User not found
+ *       500:
+ *         description: Server error
+ */
+
 export default router
--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -6,4 +6,73 @@ const router = express.Router()
 router.get('/', getMovies)
 router.get('/:id', getMovieById)
 
+/**
+ * @swagger
+ * /movie:
+ *   get:
+ *     summary: Get all movies or search by name
+ *     tags:
+ *       - Movies
+ *     parameters:
+ *       - in: query
+ *         name: name
+ *         schema:
+ *           type: string
+ *         description: The name of the movie to search for
+ *     responses:
+ *       200:
+ *         description: A list of movies
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                   name:
+ *                     type: string
+ *                   genre:
+ *                     type: string
+ *                   year:
+ *                     type: integer
+ *       404:
+ *         description: No movies found
+ */
+
+/**
+ * @swagger
+ * /movie/{id}:
+ *   get:
+ *     summary: Get a movie by ID
+ *     tags:
+ *       - Movies
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: The ID of the movie
+ *     responses:
+ *       200:
+ *         description: A single movie
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                 name:
+ *                   type: string
+ *                 genre:
+ *                   type: string
+ *                 year:
+ *                   type: integer
+ *       404:
+ *         description: Movie not found
+ */
+
 export default router
