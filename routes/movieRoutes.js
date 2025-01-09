ef62f55380ca25a7f7807637980b5abb7767a0de
vovan101207 <shiryayev_vova@bk.ru>
2025-01-09 13:59:06 +0300

    1.9

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import { getMovies, getMovieById } from '../controllers/MovieController.js'
+import { checkApiPassword } from '../middlewares/checkApiPassword.js'
 
 const router = express.Router()
 
-router.get('/', getMovies)
-router.get('/:id', getMovieById)
+router.get('/', checkApiPassword, getMovies)
+router.get('/:id', checkApiPassword, getMovieById)
 
 export default router
