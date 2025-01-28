59f742acb662ac675fe89d91a1c571f02a6e3e16
vovan101207 <shiryayev_vova@bk.ru>
2025-01-28 23:23:39 +0300

    3.9

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -7,11 +7,19 @@ export const register = async (req, res) => {
 	try {
 		const user = await registerUser(req.body)
 		const token = createToken(user._id)
-		res.json({ token })
+		const { passwordHash, ...userWithoutPassword } = user._doc
+		res.json({ token, user: userWithoutPassword })
 		sendEmail(
 			user.email,
 			'Добро пожаловать в MovieLane',
-			`Добро пожаловать в MovieLane, ${user.name}!\n\nМы рады приветствовать вас в нашем сообществе киноманов! Теперь вы можете наслаждаться лучшими фильмами прямо сейчас на нашем сайте.\n\nПриятного просмотра!\n\nКоманда MovieLane\nТехподдержка - movielane@yandex.ru`
+			`Добро пожаловать в MovieLane, ${user.name}!
+
+Мы рады приветствовать вас в нашем сообществе киноманов! Теперь вы можете наслаждаться лучшими фильмами прямо сейчас на нашем сайте.
+
+Приятного просмотра!
+
+Команда MovieLane
+Техподдержка - movielane@yandex.ru`
 		)
 	} catch (error) {
 		res
@@ -23,11 +31,20 @@ export const register = async (req, res) => {
 export const login = async (req, res) => {
 	try {
 		const { token, user } = await loginUser(req.body.email, req.body.password)
-		res.json({ token })
+		const { passwordHash, ...userWithoutPassword } = user._doc
+		res.json({ token, user: userWithoutPassword })
 		sendEmail(
 			user.email,
 			'Успешный вход на MovieLane',
-			`Привет, ${user.name}!\n\nВы успешно вошли в систему.\n\nЕсли это были не вы, пожалуйста, свяжитесь с нашей поддержкой немедленно.\n\nПриятного дня!\nКоманда MovieLane\nТехподдержка - movielane@yandex.ru`
+			`Привет, ${user.name}!
+
+Вы успешно вошли в систему.
+
+Если это были не вы, пожалуйста, свяжитесь с нашей поддержкой немедленно.
+
+Приятного дня!
+Команда MovieLane
+Техподдержка - movielane@yandex.ru`
 		)
 	} catch (error) {
 		res.status(400).json({ message: 'Ошибка при входе', error: error.message })
@@ -40,7 +57,8 @@ export const getMe = async (req, res) => {
 		if (!user) {
 			return res.status(404).json({ message: 'Пользователь не найден' })
 		}
-		res.json(user)
+		const token = createToken(user._id)
+		res.json({ user, token })
 	} catch (error) {
 		res.status(500).json({
 			message: 'Не удалось получить данные пользователя',
