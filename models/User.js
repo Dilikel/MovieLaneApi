187fa128263b5daaf93350e5226ca9522877f238
vovan101207 <shiryayev_vova@bk.ru>
2025-03-31 23:21:44 +0300

    4.4

--- models/User.js
+++ models/User.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema(
 		name: { type: String, required: true },
 		age: { type: Number, required: true },
 		email: { type: String, required: true, unique: true },
+		isSubscribed: { type: Boolean, default: false },
 		passwordHash: { type: String, required: true },
 		movieTimes: [
 			{
